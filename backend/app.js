129901a67395a0fdd25ea89c0d7d60ae6900726a
Jeanaica Suplido <sjeanaica@gmail.com>
2022-05-19 06:35:57 +0800

    ✨ (Backend): Add backend mock
    Include generation of mock data

---
+++ backend/app.js
@@ -0,0 +1,29 @@
+const { faker } = require('@faker-js/faker');
+
+const fs = require("fs");
+
+const generateCats = (num) => {
+    const cats = [];
+    
+    while (num >= 0) {
+        const gender = faker.name.gender(true)
+        const lastName = faker.name.lastName()
+        cats.push({
+            id: num,
+            name: `${faker.name.firstName(gender)} ${lastName}`,
+            breed: faker.animal.cat(),
+            owner: `${faker.name.firstName()} ${lastName}`,
+            image: faker.image.cats(400, 400, true),
+            gender: gender,
+            age: faker.datatype.number({ min: 1, max: 10})
+        });
+        num--;
+    }
+
+    return cats;
+}
+
+fs.writeFileSync(
+    "./db.json",
+    JSON.stringify({ cats: generateCats(20) })
+);
