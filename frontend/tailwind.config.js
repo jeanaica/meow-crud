c08e2f56fb6fbaa69f19b667f75ddad6b730b3ba
Jeanaica Suplido <sjeanaica@gmail.com>
2022-05-21 18:31:28 +0800

    💄 (Theme): Add theming
    Add fonts and set theme colors

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,9 +1,58 @@
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    colors: {
+      accent: {
+        DEFAULT: '#f9cfbc',
+        100: '#fdf1eb',
+        200: '#fbe3d7',
+        300: '#fad5c3',
+        400: '#c7a696',
+        500: '#957d71',
+      },
+      primary: {
+        DEFAULT: '#5c5168',
+        100: '#efeef0',
+        200: '#cecbd2',
+        300: '#aea8b4',
+        400: '#8d8595',
+        500: '#6c6277',
+        600: '#4a4153',
+        700: '#403949',
+        800: '#37313e',
+        900: '#2e2934',
+      },
+      secondary: {
+        DEFAULT: '#7f7f7f',
+        100: '#b2b2b2',
+        200: '#999999',
+        300: '#8c8c8c',
+        400: '#4c4c4c',
+        500: '#333333',
+      },
+    },
+    extend: {
+      typography: theme => ({
+        DEFAULT: {
+          css: {
+            color: theme.primary,
+            a: {
+              color: theme.secondary,
+              '&:hover': {
+                color: theme('secondary.100'),
+              },
+            },
+          },
+        },
+      }),
+      fontFamily: {
+        sans: ['"Roboto Flex"', 'sans-serif'],
+        heading: ['learning-curve', 'sans-serif'],
+      },
+      borderColor: {
+        DEFAULT: '#707070',
+      },
+    },
   },
-  plugins: [],
-}
+  plugins: [require('@tailwindcss/typography')],
+};
