3983a4b4db0c674bf6b0732a090e63bdec277104
Jeanaica Suplido <sjeanaica@gmail.com>
2022-05-26 05:59:04 +0800

    🩹 (Frontend): Fix display of back button

--- frontend/src/components/header/Header.tsx
+++ frontend/src/components/header/Header.tsx
@@ -1,18 +1,21 @@
 import Button from 'components/button/Button';
 import { FC } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { useMatch, useNavigate } from 'react-router-dom';
 
 const Header: FC = () => {
   const navigate = useNavigate();
+  const match = useMatch('/cats');
 
   return (
     <div className='flex bg-accent drop-shadow-md p-4 text-right md:text-center'>
-      <Button
-        icon
-        onClick={() => navigate(-1)}
-        className='mr-4'>
-        <span className='material-icons-outlined'>arrow_back</span>
-      </Button>
+      {!match?.pathname && (
+        <Button
+          icon
+          onClick={() => navigate(-1)}
+          className='mr-4'>
+          <span className='material-icons-outlined'>arrow_back</span>
+        </Button>
+      )}
       <h1 className='mb-0 flex-[4]'>Cat Directory</h1>
     </div>
   );
