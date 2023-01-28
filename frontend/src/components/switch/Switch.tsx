685c9e1213039b5b2d6d21c5a611ef9e92de2567
Jeanaica Suplido <sjeanaica@gmail.com>
2023-01-29 05:32:46 +0800

    💄 (Component): Update style of switch

--- frontend/src/components/switch/Switch.tsx
+++ frontend/src/components/switch/Switch.tsx
@@ -31,13 +31,13 @@ const Switch: FC<Props> = ({ id, label, value, readOnly, onChange }) => {
           checked={value}
           onChange={handleToggle}
         />
-        <div className='block w-14 h-8 bg-primary-400 rounded-full'></div>
+        <div className='block w-14 h-8 bg-slate-300 rounded-full'></div>
         <div
           className={classNames(
             'dot absolute left-1 top-1 w-6 h-6 rounded-full transition',
             {
               'bg-accent-100': !value,
-              'bg-accent translate-x-full': value,
+              'bg-green-500 translate-x-full': value,
               'cursor-not-allowed': readOnly,
             }
           )}></div>
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,47 +1,47 @@
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    colors: {
-      white: '#fff',
-      transparent: 'transparent',
-      accent: {
-        DEFAULT: '#f9cfbc',
-        100: '#fdf1eb',
-        200: '#fbe3d7',
-        300: '#fad5c3',
-        400: '#c7a696',
-        500: '#957d71',
-      },
-      primary: {
-        DEFAULT: '#5c5168',
-        100: '#efeef0',
-        200: '#cecbd2',
-        300: '#aea8b4',
-        400: '#8d8595',
-        500: '#6c6277',
-        600: '#4a4153',
-        700: '#403949',
-        800: '#37313e',
-        900: '#2e2934',
-      },
-      secondary: {
-        DEFAULT: '#7f7f7f',
-        100: '#b2b2b2',
-        200: '#999999',
-        300: '#8c8c8c',
-        400: '#4c4c4c',
-        500: '#333333',
-      },
-      error: {
-        DEFAULT: '#B00020',
-        100: '#efccd2',
-        200: '#d88090',
-        300: '#c0334d',
-        400: '#9e001d',
-        500: '#7b0016',
-      },
-    },
     extend: {
+      colors: {
+        white: '#fff',
+        transparent: 'transparent',
+        accent: {
+          DEFAULT: '#f9cfbc',
+          100: '#fdf1eb',
+          200: '#fbe3d7',
+          300: '#fad5c3',
+          400: '#c7a696',
+          500: '#957d71',
+        },
+        primary: {
+          DEFAULT: '#5c5168',
+          100: '#efeef0',
+          200: '#cecbd2',
+          300: '#aea8b4',
+          400: '#8d8595',
+          500: '#6c6277',
+          600: '#4a4153',
+          700: '#403949',
+          800: '#37313e',
+          900: '#2e2934',
+        },
+        secondary: {
+          DEFAULT: '#7f7f7f',
+          100: '#b2b2b2',
+          200: '#999999',
+          300: '#8c8c8c',
+          400: '#4c4c4c',
+          500: '#333333',
+        },
+        error: {
+          DEFAULT: '#B00020',
+          100: '#efccd2',
+          200: '#d88090',
+          300: '#c0334d',
+          400: '#9e001d',
+          500: '#7b0016',
+        },
+      },
       typography: theme => ({
         DEFAULT: {
           css: {
